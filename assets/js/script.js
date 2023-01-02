c36cd246811046a8ea8f79fe146ef2957bc2a797
Giana <gj3chavez@gmail.comgit config --global user.emailpwd>
2023-01-02 14:45:49 -0600

    js updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,81 @@
+var introSection = document.getElementById("#intro-section");
+var startButton = document.getElementById("#start");
+var questionsSection = document.getElementById("#the-questions");
+var optionsList = document.getElementById("ul");
+var finishSection = document.getElementById("#finish");
+var finalScore = document.getElementById("#final-score");
+var scoreForm = document.getElementById("#initials");
+var highScoreList = document.getElementById("#highscore-list");
+var goBackButton = document.getElementById("#go-back");
+var clearHSButton = document.getElementById("#clear-hs");
+var feedback = document.getElementById("#feedback");
+var timer = document.querySelector("#time");
+
 var score = 0;
-var HighScores =[];
-//var timeEl = document.querySelector("#time");
-
-var questionsEl = document.getElementById("");
-var initialsEl = document.getElementById("");
+var currentQuestion = 1;
+var timeLeft = 60;
+var timer = document.querySelector(".timer");
 
 
-var timerEl = document.querySelector("time");
-var questionNumber = 0;
-var timeValue = 60;
-var questionCount = 1;
+function setTimer(){
 
-//buttons
-var submitBtn= document.querySelector("")
-var startBtn = document.querySelector("#start");
+    var timerInterval = setInterval(function){
+        secondsLeft--;
+        timeLeft.textContent = "Time:"+ "s";
 
-function SetTime(){
-    var Interval = setInterval(() => {
-        timeValue = timeValue - 1;
-        timeLeft.innerHTML = timeValue;
-        if (timeValue === 0){
-           clearInterval(Interval);
+        if (secondsLeft <= 0){
+            clearInterval(timerInterval);
+            timeLeft.textContent= "All done!";
         }
 
-        
+        else if(currentQuestion >= questions.length +1){
+            clearInterval(timerInterval);
+        }
+
+    
     }, 1000);
 
 }
-function startQuiz(){
-    //write the questions here
+
+//-------------------Questions---------------------------------
+var questions =[
+    {
+        question: "Commonly used data types DO NOT Include:",
+        options: ["a.booleans", "b.strings", "c.numbers", "d.alerts"],
+        answer: "d"
+}
+    {
+        question: "The condition in an if/else statement is enclosed within____.",
+        options: ["a.quotes", "b.curly brackets", "c.parenthesis", "d.square backets"],
+        answer: "b"
+
+}
+    {
+        question: "Arrays in JavaScript can be used to store:",
+        options: ["a.booleans", "b.numbers and strings", "c.other arrays", "d.all of the avobe"],
+        answer: "d"
+    }
+    {
+        question: "String values must be enclosed within_____ when being assigned to variables.",
+        options: ["a.commas", "b.curly brackets", "c.quotes", "d.parenthesis"],
+        answer: "c"
+    }
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        options: ["a.JavaScript", "b.terminal/bash", "c.for loops", "d.console.log"],
+        answer: "d"
+    }
+]
+
+var setTime = function(){
+    timeLeft = 60;
+
+var timerchek = setInterval(function(){
+
+})
 }
 
+
+startButton.onclick = startQuiz;
+scoreForm.onclick = onkeyup = checkForEnter;
+highScoreList.onclick = saveHighscore;
