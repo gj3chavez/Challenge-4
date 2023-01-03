c2b4794f5d363d747e41c3f15dfbf12dfd2c2234
Giana <gj3chavez@gmail.comgit config --global user.emailpwd>
2023-01-02 18:50:07 -0600

    more updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var introSection = document.getElementById("#intro-section");
-var startButton = document.getElementById("#start");
+var startBtn = document.querySelector("#start");
 var questionsSection = document.getElementById("#the-questions");
-var optionsList = document.getElementById("ul");
+//var optionsList = document.getElementById("ul");
 var finishSection = document.getElementById("#finish");
 var finalScore = document.getElementById("#final-score");
 var scoreForm = document.getElementById("#initials");
@@ -11,71 +11,109 @@ var clearHSButton = document.getElementById("#clear-hs");
 var feedback = document.getElementById("#feedback");
 var timer = document.querySelector("#time");
 
-var score = 0;
-var currentQuestion = 1;
-var timeLeft = 60;
-var timer = document.querySelector(".timer");
-
-
-function setTimer(){
-
-    var timerInterval = setInterval(function){
-        secondsLeft--;
-        timeLeft.textContent = "Time:"+ "s";
-
-        if (secondsLeft <= 0){
-            clearInterval(timerInterval);
-            timeLeft.textContent= "All done!";
-        }
-
-        else if(currentQuestion >= questions.length +1){
-            clearInterval(timerInterval);
-        }
-
-    
-    }, 1000);
-
-}
 
 //-------------------Questions---------------------------------
 var questions =[
     {
         question: "Commonly used data types DO NOT Include:",
-        options: ["a.booleans", "b.strings", "c.numbers", "d.alerts"],
-        answer: "d"
+        options: {"a.booleans", "b.strings", "c.numbers", "d.alerts"},
+        correctAnswer: "d"
 }
     {
         question: "The condition in an if/else statement is enclosed within____.",
-        options: ["a.quotes", "b.curly brackets", "c.parenthesis", "d.square backets"],
-        answer: "b"
+        options: {"a.quotes", "b.curly brackets", "c.parenthesis", "d.square backets"},
+        correctAnswer: "b"
 
 }
     {
         question: "Arrays in JavaScript can be used to store:",
-        options: ["a.booleans", "b.numbers and strings", "c.other arrays", "d.all of the avobe"],
-        answer: "d"
+        options: {"a.booleans", "b.numbers and strings", "c.other arrays", "d.all of the avobe"},
+        correctAnswer: "d"
     }
     {
         question: "String values must be enclosed within_____ when being assigned to variables.",
-        options: ["a.commas", "b.curly brackets", "c.quotes", "d.parenthesis"],
-        answer: "c"
+        options: {"a.commas", "b.curly brackets", "c.quotes", "d.parenthesis"},
+        correctAnswer: "c"
     }
     {
         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        options: ["a.JavaScript", "b.terminal/bash", "c.for loops", "d.console.log"],
-        answer: "d"
+        options: {"a.JavaScript", "b.terminal/bash", "c.for loops", "d.console.log"},
+        correctAnswer: "d"
     }
 ]
-
-var setTime = function(){
-    timeLeft = 60;
-
-var timerchek = setInterval(function(){
-
+startBtn.addEventListener("click", function(){
+    document.getElementById("time").style.display = "flex";
+    document.getElementById("intro-section").style.display = "none";
+    document.getElementById("the-questions").style.display = "flex";
+    setTimer();
 })
+//-------------start timer------------
+
+var timeLeft = 60;
+var timer = document.querySelector(".time");
+
+function setTimer(){
+
+} timerInterval = setInterval(function(){
+    timeLeft--;
+    timer.textContent = timeLef;
+     
+    if (timer<= 0){
+        endQuiz();
+    }
+},1000);
+
+
+//---display first question-----
+displayQuestion();
+
+
+
+function displayQuestion(){
+    //---get current question and its answers---
+    var { question, options, correctAnswer} =
+questions[currentQuestion];
+
+    questionEl.querySelector("h2").textContent = question;
+
+    Object.keys(options).forEach(function(key){
+        var btn = questionEl.querySelector('#btnanswer${key}');
+        btn.textContent = options[key];
+
+        btn.addEventListener("click", function(){
+            checkAnswer(key);
+        });
+    });
+}
+//----------feedback section-----------------
+
+function checkAnswer(userAnswer){
+    var correctAnswer =
+questions[currentQuestion].correctAnswer;
+
+if (userAnswer=== correctAnswer){
+    feedbackEl.textContent = "Correct!";
+    score++;
+}
+else {
+    feedbackEl.textContent = "Wrong!";
+    timer-=10;
+}
+
+currentQuestion++;
+if (currentQuestion=== questions.length){
+    endQuiz();
+}
+else{
+    displayQuestion();
+}
+}
+
+function endQuiz(){
+
+    document.getElementById("finish").classList.remove("hide");
+
+    document.getElementById("final-score").textContent = "Your final score is ${score}";
 }
 
 
-startButton.onclick = startQuiz;
-scoreForm.onclick = onkeyup = checkForEnter;
-highScoreList.onclick = saveHighscore;
